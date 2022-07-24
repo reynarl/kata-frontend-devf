3c2e8b62a0484d1e732da538cc8342ef717c31ed
Reeyna <reeynarl@gmail.com>
2022-07-24 12:02:34 -0500

    [ADD] 404 route using navigate and useNavigate

--- searchMovies/src/components/Product.jsx
+++ searchMovies/src/components/Product.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
 import { products } from '../data'
 
 const Product = () => {
   const { productId } = useParams()
+  const navigate = useNavigate()
 
   // 1. hacemos un find para que encuentre la primera coincidencia en products donde el id de products, sea igual a productId.
   // 2. convertimos a number porque productId era String
@@ -11,12 +12,16 @@ const Product = () => {
   const uniqueProduct = products.find(item => item.id === Number(productId))
   console.log(uniqueProduct)
 
+  useEffect(() => {
+    uniqueProduct === undefined && navigate('/404')
+  }, [])
+
   return (
     <article>
-      <img src={uniqueProduct.thumbnail} alt={uniqueProduct.title} className='img-fluid' height={150} style={{ objectFit: 'cover' }} />
+      <img src={uniqueProduct?.thumbnail} alt={uniqueProduct?.title} className='img-fluid' height={150} style={{ objectFit: 'cover' }} />
       <div>
-        <p>{uniqueProduct.title}</p>
-        <p>{uniqueProduct.description}</p>
+        <p>{uniqueProduct?.title}</p>
+        <p>{uniqueProduct?.description}</p>
       </div>
     </article>
   )
--- searchMovies/src/routes.jsx
+++ searchMovies/src/routes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Outlet, Navigate } from 'react-router-dom'
 import App from './components/App'
 import Nabvar from './components/Nabvar'
 import Products from './components/Products'
@@ -12,12 +12,22 @@ const Paths = () => {
       <Routes>
         {/* Ctrl + espacio para saber que puede recibir ese componente */}
         <Route path='/' element={<p>Home</p>} />
-        <Route path='products' element={<Products />} />
+
         {/* Crear ruta dinámica con useParams()
-            1. le mandamos en path una clave única(id) en este caso lo llamamos :productId  siempre lleva los : 
+            1. le mandamos en path una clave única(id) en este caso lo llamamos :productId  siempre lleva los :
             2. Luego haremos la ruta dinámica dentro products.jsx */}
-        <Route path='products/:productId' element={<Product />} />
-        <Route path='movies' element={<App />} />
+        {/* <Route path='products/:productId' element={<Product />} /> */}
+        {/* RUTAS ANIDADAS (Nested routes) CON Outlet -> ejem. /products/3 */}
+        <Route path='/products' element={<Outlet />}> {/* <--Elemento padre */}
+          <Route> {/* <--Elemento hijo donde estarán todas las rutas heredan de padre */}
+            <Route index element={<Products />} /> {/* <- Elemento principal */}
+            <Route path=':productId' element={<Product />} />
+          </Route>
+        </Route>
+
+        <Route path='/movies' element={<App />} />
+        <Route path='/404' element={<p>404</p>} />
+        <Route path='*' element={<Navigate to='/404' />} /> {/* <- El *  hace referencia a si no existe esa ruta */}
       </Routes>
     </section>
   )
